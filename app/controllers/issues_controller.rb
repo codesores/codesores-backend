1d2c862b17f3f0f43addfd66e99f837866efcbcb
john-michael <j.johnmichaelmurphy@gmail.com>
2017-07-28 11:46:27 -0700

    add CORS access and set up MVP routes for API

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,20 @@
+class  IssuesController < ApplicationController
+
+  def index
+    @test = ['hi']
+    render json: @test
+  end
+
+  def show
+
+  end
+
+  def search
+
+  end
+
+  def results
+
+  end
+
+end

edcdb31647c0eabb7052b194d41346bf88a583eb
john-michael <j.johnmichaelmurphy@gmail.com>
2017-07-28 15:06:13 -0700

    finish CORS setup

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,12 +1,32 @@
 class  IssuesController < ApplicationController
 
   def index
-    @test = ['hi']
-    render json: @test
+
   end
 
   def show
+    @test_response = {
+      issue: {
+        summary: 'there is something wrong with something',
+        comments: [
+          {comment: 'this is a comment',
+            author: 'JM'
+          },
+          {comment: 'this is another comment',
+            author: 'JM'
+          }
+        ],
+        meta: {
+          labels: ['label1', 'label2']
+        }
+      },
 
+      feedback: {
+        data: 'data'
+      }
+    }
+
+    render json: @test_response
   end
 
   def search

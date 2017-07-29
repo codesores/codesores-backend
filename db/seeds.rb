c0cf7c5f0910c1f8e7aee81af4a3cf987d10089d
Gustavo Amaral <gustavoamaral@Gustavos-MacBook-Pro.local>
2017-07-28 19:36:52 -0700

    corrected your horrible SPELLING, and organized issues seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,52 @@
 require_relative 'fake_server_response'
 
-Repos.delete_all
-Issues.delete_all
+# Repos.delete_all
+# Issues.delete_all
 
- @repos[:data][:search][:edges].each do |repo|
-   repo = repo[:node]
 
-   Repo.new(
-   :name=>"freeCodeCamp",
-   :url=>"https://github.com/freeCodeCamp/freeCodeCamp",
-   :owner[:login],
-   :description=>"The https://freeCodeCamp.com open source codebase and curriculum. Learn to code and help nonprofits.",
-   :primaryLanguage=>{:name=>"JavaScript"},
-   :mentionableUsers[:totalCount]
-   :stargazers[:totalCount]
-   :issues=[:totalCount]
-   :forks[:totalCount]
-   :pullRequests[:totalCount]
-   :updatedAt=>"2017-07-28T21:18:32Z"}
-   )
+@freecodecamp_issues[:data][:repository][:issues][:edges].each do |obj|
 
- end
+   obj.each do |key, value|
+      p value[:title]
+      p value[:labels][:edges]
+      p value[:createdAt]
+      p value[:comments][:totalCount]
+      p value[:url]
+      p value[:author][:login]
+      p value[:participants][:totalCount]
+      p value[:assignees][:totalCount]
+      p value[:repository][:nameWithOwner].match(/\/.*/).to_s[(1..-1)]
+   end
+end
+
+@webpack_issues[:data][:repository][:issues][:edges].each do |obj|
+
+   obj.each do |key, value|
+      p value[:title]
+      p value[:labels][:edges]
+      p value[:createdAt]
+      p value[:comments][:totalCount]
+      p value[:url]
+      p value[:author][:login]
+      p value[:participants][:totalCount]
+      p value[:assignees][:totalCount]
+      p value[:repository][:nameWithOwner].match(/\/.*/).to_s[(1..-1)]
+   end
+end
+
+@modernizr_issues[:data][:repository][:issues][:edges].each do |obj|
+
+   obj.each do |key, value|
+      p value[:title]
+      p value[:labels][:edges]
+      p value[:createdAt]
+      p value[:comments][:totalCount]
+      p value[:url]
+      p value[:author][:login]
+      p value[:participants][:totalCount]
+      p value[:assignees][:totalCount]
+      p value[:repository][:nameWithOwner].match(/\/.*/).to_s[(1..-1)]
+   end
+end
+
+

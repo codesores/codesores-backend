ba5918ba01bf3e84ec38facb892558613cf65dae
Josh Leichtung <joshleichtung@gmail.com>
2017-07-31 14:36:40 -0700

    Add repo, language, and comments to Issue json

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,8 @@ class Issue < ApplicationRecord
   belongs_to :repo
   has_one :language, through: :repo, source: :language
   has_many :user_feedbacks
+
+  def as_json(options = {})
+    super(include: [:repo, :language, :user_feedbacks])
+  end
 end

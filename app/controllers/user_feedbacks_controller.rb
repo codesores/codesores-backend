970e481180335458ce0053cdea872b9effd27cb8
satong <stephanietong@gmail.com>
2017-08-02 14:39:15 -0700

    Add in logic to update issue request type based on user feedback after threshold

--- app/controllers/user_feedbacks_controller.rb
+++ app/controllers/user_feedbacks_controller.rb
@@ -7,6 +7,18 @@ class UserFeedbacksController < ApplicationController
     @feedback.user = current_user
 
     @feedback.save!
+
+    target_issue = @feedback.issue
+    if target_issue.user_feedbacks.count > 15
+      # Get mode of request_type classification made by users
+      request_type_inputs = target_issue.user_feedbacks.pluck(:request_type_id).sort
+      freq = request_type_inputs.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+      request_type_id_users = request_type_inputs.max_by { |v| freq[v] }
+      # Set mode as the issue.request_type.id, overwriting classifcation prediction made by Watson. By default, will take the first integer if there are multiple modes.
+      target_issue.request_type_id = request_type_id_users
+      target_issue.save!
+    end
+
     render json: ['hii']
   end
 

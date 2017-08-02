2fe5dd800de302265c8443569ba0f608ea1a5296
Josh Leichtung <joshleichtung@gmail.com>
2017-08-02 16:01:10 -0700

    Refactor Issue to only include info necessary for views

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -5,26 +5,26 @@ class  IssuesController < ApplicationController
     @languages = Language.all.pluck(:language)
     # @issues = Issue.most_recent(10)
     @issues = Issue.hot_issues
-    # render json: @languages, @issues
-    render :json => {
-      :languages => @languages.as_json,
-      :issues => @issues.as_json
+    render json: {
+      languages: @languages.as_json,
+      issues: @issues.as_json(include: :repo)
     }
   end
 
   def index
-    render json: @issues
+    @issues = Issue.includes(:repo).limit(20)
+    render json: {issues: @issues, repo: @issues.repo}
   end
 
   def show
-    issue = Issue.includes(:repo, :language, :user_feedbacks).find(params[:id])
-    render json: issue
+    issue = Issue.includes(:repo, :language, :user_feedbacks, :stars).find(params[:id])
+    render json: {issue: issue, feedbacks: issue.user_feedbacks, repo: issue.repo, stars: issue.stars}
   end
 
   def search
     @issues = Issue.advanced_search(params['bugs'], params['documentation'], params['language'], params['difficulty'].to_i, params["keywords"])
 
-    render json: @issues
+    render json: @issues.as_json(include: :repo)
   end
 
   def results
